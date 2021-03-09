b1658925722d8463a4f9eb1f6f654c5176eab63b
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-03-09 12:11:37 +0100

    I added the showGameStatistics method, which gets, generates and returns the amount of games, won and lost, returning them

--- src/js/Statistics.js
+++ src/js/Statistics.js
@@ -13,7 +13,15 @@ class Statistics {
         console.log(gameResult);
         this.gameResults.push(gameResult);
     }
+
+    showGameStatistics() {
+        let games = this.gameResults.length; // ilosc gier łącznie
+        let wins = this.gameResults.filter(result => result.win).length; // szukam wartosci true za pomoca filter
+        let losses = this.gameResults.filter(result => !result.win).length; // szukam wartosci false za pomoca filter
+        console.log(games, wins, losses);
+        return [games, wins, losses];
+    }
 }
 
-const stats = new Statistics();
-console.log(stats);
+// const stats = new Statistics();
+// console.log(stats);
