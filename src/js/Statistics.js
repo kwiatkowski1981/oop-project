1f60301d2ed4e0f49c05134212ab3e9ec3dc3b42
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-03-09 11:45:09 +0100

    I added a gameResult property that will keep the game and bet value in it. I added a method in the addGameToStatistics prototype that adds an object to the gameResults array that holds the game & bid properties, where game is a bolean value (true or false) and the bid value will be expressed as number.

--- src/js/Statistics.js
+++ src/js/Statistics.js
@@ -1,8 +1,19 @@
 class Statistics {
 
     constructor() {
-
+        this.gameResults = [];
 
     }
 
+    addGameToStatistics(win, bid) {
+        let gameResult = {
+            win: win,
+            bid: bid,
+        }
+        console.log(gameResult);
+        this.gameResults.push(gameResult);
+    }
 }
+
+const stats = new Statistics();
+console.log(stats);
