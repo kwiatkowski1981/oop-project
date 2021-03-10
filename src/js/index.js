c7e7bb942a812913da0a67adcd245561377ec621
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-03-10 15:59:19 +0100

    Getting all the page elements and classes needed to work on them.

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,28 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-// console.log('HELLO 🚀')
+console.log('HELLO 🚀')
+
+class Game {
+    constructor() {
+        this.stats = new Statistics();
+        this.wallet = new Wallet(100);
+        document.getElementById('start').addEventListener('click', this.startGame());
+        this.spanWallet = document.querySelector('.panel span.wallet');
+        this.boards = document.querySelectorAll('div.color');
+        this.inputBid = document.getElementById('bid');
+        this.spanResult = document.querySelector('score span.result');
+        this.spanGames = document.querySelector('score span.number');
+        this.spanWins = document.querySelector('score span.win');
+        this.spanLosses = document.querySelector('score span.loss');
+    }
+
+
+    render() {
+
+    }
+
+    startGame() {
+
+    }
+}
