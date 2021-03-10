89603cef55cc765325e471a3945d0f562f221229
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-03-10 12:07:09 +0100

    I am creating a method that will give me access to a variable with the results of drawing colors

--- src/js/Draw.js
+++ src/js/Draw.js
@@ -3,8 +3,13 @@ class Draw {
     constructor() {
 
         this.options = ["red", "green", "blue"];
-        let _result = ["red", "green", "blue"];
+        let _result = this.drawResult();
 
         this.getDrawResult = () => _result;
+    }
+    drawResult (){
+        let colors = [];
+
+        return colors;
     }
 }
