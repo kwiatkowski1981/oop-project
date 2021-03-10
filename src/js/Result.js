bd306f1c2c76ac4e53e02ccfd716e50a7060911f
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-03-10 15:26:33 +0100

    I define (the rules of the game) when the game is won and when the game is lost.

--- src/js/Result.js
+++ src/js/Result.js
@@ -5,8 +5,12 @@ class Result {
         else return 0;
     }
 
+    static checkWinner(draw) {
+        if (draw[0] === draw[1] && draw[1] === draw[2] ||
+            draw[0] !== draw[1] && draw[1] !== draw[2] ||
+            draw[0] !== draw[2]) return true;
+        else return false;
 
     }
+
 }
-
-
